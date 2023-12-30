0afd3b4fe0ea86afb884ea89fce4657884d7787b
Yunus Isler <yunusisler91@gmail.com>
2023-12-30 21:34:49 +0300

    tablolardan veri cekip sitenin bolum adlı tabında gosterme hakkında ilk deneme gerceklestirildi

---
+++ Hastanevs/Hastanevs/Controllers/BolumController.cs
@@ -0,0 +1,23 @@
+﻿using Hastanevs.Models;
+using Hastanevs.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hastanevs.Controllers
+{
+    public class BolumController : Controller
+    {
+        //dependency injection özelliği sayesinde new anahtarı ile yeni nesne türetmeden containerdaki nesneyi enjekte etmiş oluyoruz.
+        //program.cs dosyasındaki service kısmına yazdığımız kodlar bunu sağladı.
+        private readonly HastaneDbContext _hastaneDbContext;
+
+        public BolumController(HastaneDbContext context)
+        {
+            _hastaneDbContext = context;
+        }
+        public IActionResult Index()
+        {
+            List<Bolum> objBolumList = _hastaneDbContext.Bolumler.ToList();
+            return View(objBolumList);
+        }
+    }
+}
