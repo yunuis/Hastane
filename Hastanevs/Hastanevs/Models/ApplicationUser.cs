a7a61dc5fdaf9180b57c32850cedb8ec6167ad0d
Yunus Isler <yunusisler91@gmail.com>
2024-01-25 18:19:07 +0300

    yanlıslıkla yuklenmis dosyalar kaldırıldı

--- Hastanevs/Hastanevs/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Hastanevs.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
--- Hastanevs/Hastanevs/Utility/HastaneDbContext.cs
+++ Hastanevs/Hastanevs/Utility/HastaneDbContext.cs
@@ -1,17 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Hastanevs.Models;
+﻿using Hastanevs.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hastanevs.Utility
 {   
     //DbContext sınıfından nesnemizi oluşturuyoruz.Bu nesne entitiy'lerimiz ve veritabanı arasında köprü işlevi görecek. 
-    public class HastaneDbContext : IdentityDbContext
+    public class HastaneDbContext : DbContext
     {
         public HastaneDbContext(DbContextOptions<HastaneDbContext> options) : base(options) { }
 
         //entity frameworkde bolum adlı sınıfa tekabul eden veritabanındaki tablomuzun ismi bolumler olacak şekilde ayarlıyoruz.
         public DbSet<Bolum> Bolumler { get; set; }
-
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
